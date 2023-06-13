78a5a8117907143494fdf34001dcc2ef7579dee3
hungnpvce160687 <hungnpvce160687@fpt.edu.vn>
2023-06-13 15:15:49 +0700

    map2 update

--- Assets/Scripts/Common/EnemyController.cs
+++ Assets/Scripts/Common/EnemyController.cs
@@ -61,7 +61,8 @@ public class EnemyController : MonoBehaviour
         m_FacingRight = !m_FacingRight;
         // Multiply the player's x local scale by -1.
         Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
+        //theScale.x *= -1;
+        theScale.x *= 1;
         transform.localScale = theScale;
     }
 
