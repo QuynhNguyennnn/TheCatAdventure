4aa933e3d16653a9e7639e2ad050d47211f7b32f
Quynhcuteeee <quynhnguyenst1610@gmail.com>
2023-06-23 17:17:50 +0700

    map5 fix commit - CongratulationController on error

---
+++ Assets/Scripts/Map5/Attack1_5.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack1_5 : MonoBehaviour
+{
+    PlayerController player;
+    public int damageToGive = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy") && player.isAttack1())
+        {
+            EnemyHealthManagement enemyHealth;
+            enemyHealth = collision.GetComponent<EnemyHealthManagement>();
+            enemyHealth.HurtEnemy(damageToGive);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy") && player.isAttack1())
+        {
+            EnemyHealthManagement enemyHealth;
+            enemyHealth = collision.GetComponent<EnemyHealthManagement>();
+            enemyHealth.HurtEnemy(damageToGive);
+        }
+    }
+}
